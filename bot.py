9a7a5a6afd9e60b13e282b5ddd75186e676dfbb0
Nicolai Rendtslev <nre@caliodesign.com>
2019-04-29 12:58:10 +0200

    added server prints

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ authorized_servers = [str(line) for line in r if len(line) !=0]
 #     if len(line) != 0:
 #         authorized_servers.append(str(line))
 print('Authorized servers are: \n')
-servers = map(lambda x: print(x.name), bot.get_guild(authorized_servers))
+servers = map(print(x.name), bot.get_guild(authorized_servers))
 
 
 # Guild joining server
