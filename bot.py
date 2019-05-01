3b77b687ea20e192dc62371a839da34c469e89dd
Nicolai Rendtslev <nre@caliodesign.com>
2019-05-02 00:48:20 +0200

    added working channel command

--- bot.py
+++ bot.py
@@ -265,7 +265,7 @@ async def on_message(message):
     #
     elif message.content.startswith('!sschannel'):
         await message.channel.send(f'Working channel is {channels[0]}')
-        print(message.author.guild_permissions)
+        print(message.author.guild_permissions.administrator)
     elif str(message.channel) in channels:
         # Check start of messages if they contain sscommands command.
         if message.content.startswith('!addstreamer'):
